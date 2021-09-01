b67b14be33100436cc40945b50cd9147e35a787f
vhchauu <vuhachau2412@gmail.com>
2021-09-01 23:27:47 +0700

    main: checked

---
+++ js/ques.js
@@ -0,0 +1,62 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Xin chào",
+    img: "topright",
+    answer: "Đã hiểu",
+    options: [
+      "Đã hiểu",
+      // "Màu cam",
+      // "Màu xanh",
+      // "Màu hồng",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Tạm biệt",
+    img: "topright",
+    answer: "Đã hiểu",
+    options: [
+      "Đã hiểu"
+      // "Tạm biệt",
+      // "Chữ F",
+      // "Chữ A",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Đây là ký hiệu gì?",
+    img: "tambiet.mp4",
+    answer: "Tạm biệt",
+    options: [
+      "Tạm biệt",
+      "Xin chào",
+      // "Chữ H",
+      // "Chữ D",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Đây là ký hiệu gì?",
+    img: "xinchao.mp4",
+    answer: "Xin chào",
+    options: [
+      "Tạm biệt",
+      "Xin chào",
+      // "Chữ K",
+      // "Chữ D"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Đây là ký hiệu gì?",
+    img: "xinchao.mp4",
+    answer: "Xin chào",
+    options: [
+      "Tạm biệt",
+      "Xin chào",
+      // "Chữ C",
+      // "Chữ M",
+    ]
+  }
+];
