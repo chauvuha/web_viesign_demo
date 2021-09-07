2b5b80af90206f0794abb9b6c3942ccf72ff8373
vhchauu <vuhachau2412@gmail.com>
2021-09-07 17:07:29 +0700

    added

---
+++ js/ques_level4_2.js
@@ -0,0 +1,145 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Đâu là ký hiệu chỉ vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/vui.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/tứcgiận.mp4",
+    options: [
+      "Lo lắng",
+      "Tức giận",
+    ]
+  },
+  {
+    numb: 3,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Sợ hãi",
+    source: "../videos/sợ.mp4",
+    options: [
+      "Sợ hãi",
+      "Xấu hổ",
+    ]
+  },
+  {
+    numb: 4,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Vui vẻ",
+    source: "../videos/vui.mp4",
+    options: [
+      "Vui vẻ",
+      "Lo lắng",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Đâu là ký hiệu chỉ Buồn?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/buồn.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+  {
+    numb: 6,
+    question: "Đâu là ký hiệu chỉ Tức giận?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/tứcgiận.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/vui.mp4",
+    ]
+  },
+  {
+    numb: 7,
+    question: "Đâu là ký hiệu chỉ Vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/vui.mp4",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Đâu là ký hiệu chỉ vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/vui.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+  {
+    numb: 9,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/tứcgiận.mp4",
+    options: [
+      "Lo lắng",
+      "Tức giận",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Đây là ký hiệu chỉ câu đơn gì?",
+    img: "type1",
+    answer: "Tôi cảm thấy vui",
+    source: "../videos/Toidangcamthayvui.mp4",
+    options: [
+      "Tôi cảm thấy vui",
+      "Tôi cảm thấy tức giận",
+    ]
+  },
+  {
+    numb: 11,
+    question: "Đây là ký hiệu chỉ câu phức gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/[Đ]Khongtoikhongcamthaynhuvay.mp4",
+    options: [
+      "Không,tôi không cảm thấy như vậy",
+      "Không, tôi đang tức giận",
+    ]
+  },
+  {
+    numb: 12,
+    question: "Chọn câu đáp phù hợp cho ký hiệu dưới đây",
+    img: "type3",
+    answer: "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+    source: "../videos/Bancodangcamthayvuikhong.mp4",
+    options: [
+      "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+      "../videos/[Đ]Toicungvay.mp4",
+    ]
+  },
+  {
+    numb: 13,
+    question: "Chọn câu đáp phù hợp cho ký hiệu dưới đây",
+    img: "type3",
+    answer: "../videos/[Đ]Toicungvay.mp4",
+    source: "../videos/Toidangcamthayvui.mp4",
+    options: [
+      "../videos/[Đ]Toicungvay.mp4",
+      "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+    ]
+  },
+];
---
+++ js/ques_level5_1.js
@@ -0,0 +1,178 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Đâu là ký hiệu chỉ vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/vui.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/tứcgiận.mp4",
+    options: [
+      "Lo lắng",
+      "Tức giận",
+    ]
+  },
+  {
+    numb: 3,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Sợ hãi",
+    source: "../videos/sợ.mp4",
+    options: [
+      "Sợ hãi",
+      "Xấu hổ",
+    ]
+  },
+  {
+    numb: 4,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Vui vẻ",
+    source: "../videos/vui.mp4",
+    options: [
+      "Vui vẻ",
+      "Lo lắng",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Đâu là ký hiệu chỉ Buồn?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/buồn.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+  {
+    numb: 6,
+    question: "Đâu là ký hiệu chỉ Tức giận?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/tứcgiận.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/vui.mp4",
+    ]
+  },
+  {
+    numb: 7,
+    question: "Đâu là ký hiệu chỉ Vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/tứcgiận.mp4",
+      "../videos/vui.mp4",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Đâu là ký hiệu chỉ vui?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/vui.mp4",
+    options: [
+      "../videos/vui.mp4",
+      "../videos/buồn.mp4",
+    ]
+  },
+  {
+    numb: 9,
+    question: "Đây là ký hiệu gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/tứcgiận.mp4",
+    options: [
+      "Lo lắng",
+      "Tức giận",
+    ]
+  },
+  {
+    numb: 10,
+    question: "Đây là ký hiệu chỉ câu đơn gì?",
+    img: "type1",
+    answer: "Tôi cảm thấy vui",
+    source: "../videos/Toidangcamthayvui.mp4",
+    options: [
+      "Tôi cảm thấy vui",
+      "Tôi cảm thấy tức giận",
+    ]
+  },
+  {
+    numb: 11,
+    question: "Đây là ký hiệu chỉ câu đơn gì?",
+    img: "type1",
+    answer: "Có",
+    source: "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+    options: [
+      "Tôi cũng vậy",
+      "Có",
+    ]
+  },
+  {
+    numb: 12,
+    question: "Đây là ký hiệu chỉ câu ghép gì?",
+    img: "type1",
+    answer: "Tức giận",
+    source: "../videos/[Đ]Khongtoikhongcamthaynhuvay.mp4",
+    options: [
+      "Không,tôi không cảm thấy như vậy",
+      "Không, tôi đang tức giận",
+    ]
+  },
+  {
+    numb: 13,
+    question: "Đây là ký hiệu chỉ câu ghép gì?",
+    img: "type1",
+    answer: "Không,tôi không cảm thấy như vậy",
+    source: "../videos/[Đ]Khongtoikhongcamthaynhuvay.mp4",
+    options: [
+      "Không,tôi không cảm thấy như vậy",
+      "Không, tôi đang tức giận",
+    ]
+  },
+  {
+    numb: 14,
+    question: "Chọn câu đáp phù hợp cho ký hiệu dưới đây",
+    img: "type3",
+    answer: "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+    source: "../videos/Bancodangcamthayvuikhong.mp4",
+    options: [
+      "../videos/[Đ]Cotoidangcamthaynhuvay.mp4",
+      "../videos/[Đ]Toicungvay.mp4",
+    ]
+  },
+  {
+    numb: 15,
+    question: "Đây là cấu trúc gì?",
+    img: "type1",
+    answer: "Tôi đang cảm thấy...",
+    source: "../videos/[Đ]Toidangcamthay....mp4",
+    options: [
+      "Tôi đang cảm thấy...",
+      "Bạn có đang cảm thấy....",
+    ]
+  },
+  {
+    numb: 16,
+    question: "Đâu là ký hiệu chỉ Bạn có đang cảm thấy... không?",
+    img: "type2",
+    source: "#",
+    answer: "../videos/[Đ]Bancodangcamthay...khong.mp4",
+    options: [
+      "../videos/[Đ]Bancodangcamthay...khong.mp4",
+      "../videos/vui.mp4",
+    ]
+  },
+];
