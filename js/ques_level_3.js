956c3961e648b05a494949d1b886571cbdac0ddb
vhchauu <vuhachau2412@gmail.com>
2021-09-12 17:07:17 +0700

    dine

--- js/ques_level_3.js
+++ js/ques_level_3.js
@@ -4,7 +4,8 @@ let questions = [
     question: "Chào bạn, hôm nay bạn cảm thấy thế nào?",
     img: "understand",
     // source: "../videos/phan3/Phần_3_Chào_bạn_hôm_nay_bạn_cảm_thấy.mp4",
-    source: "../videos/phan2/Phần_2_Ấn_tượng.mp4",
+    // source: "../videos/phan2/Phần_2_Ấn_tượng.mp4",
+    source: "../videos/phan2/Phần_3_Chào.mp4",
     answer: "Đã hiểu",
     options: [
       "Đã hiểu",
